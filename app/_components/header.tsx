f49b3ea340f6cc3ab952ffad6ad285124b3773b9
carlosvbp <CarlosVinicius7345@gmail.com>
2024-08-11 21:43:56 -0300

    feat: ensure barbershops can be searched by title and service

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -4,13 +4,16 @@ import { MenuIcon } from "lucide-react"
 import Image from "next/image"
 import { Sheet, SheetTrigger } from "./ui/sheet"
 import SidebarSheet from "./sidebar-sheet"
+import Link from "next/link"
 
 const Header = () => {
   return (
     <div>
       <Card>
         <CardContent className="flex flex-row items-center justify-between p-5">
-          <Image alt="FSW Barber" src="/logo.png" height={18} width={120} />
+          <Link href="/">
+            <Image alt="FSW Barber" src="/logo.png" height={18} width={120} />
+          </Link>
           <Sheet>
             <SheetTrigger asChild>
               <Button size="icon" variant="outline">
--- app/_components/sidebar-sheet.tsx
+++ app/_components/sidebar-sheet.tsx
@@ -89,19 +89,19 @@ const SidebarSheet = () => {
       </div>
       <div className="flex flex-col gap-1 border-b border-solid py-5">
         {quickSearchOptions.map((option) => (
-          <Button
-            key={option.title}
-            className="justify-start gap-2"
-            variant="ghost"
-          >
-            <Image
-              src={option.imageUrl}
-              alt={option.title}
-              height={18}
-              width={18}
-            />
-            {option.title}
-          </Button>
+          <SheetClose key={option.title} asChild>
+            <Button className="justify-start gap-2" variant="ghost" asChild>
+              <Link href={`/barbershops?service=${option.title}`}>
+                <Image
+                  src={option.imageUrl}
+                  alt={option.title}
+                  height={18}
+                  width={18}
+                />
+                {option.title}
+              </Link>
+            </Button>
+          </SheetClose>
         ))}
       </div>
       <div className="flex flex-col gap-1 py-5">
